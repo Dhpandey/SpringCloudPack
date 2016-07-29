dcbacfbc29dc266893fd6db347f5a0df8c8e6738
Dheeraj Pandey <dheerazpandey@gmail.com>
2016-07-29 09:49:27 -0500

    streamsource renamed

---
+++ StreamSourceDemo/src/main/java/com/streamdemo/StreamdemoApplication.java
@@ -0,0 +1,12 @@
+package com.streamdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamdemoApplication.class, args);
+	}
+}
